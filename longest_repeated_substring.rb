da1432f050280194eb4e6a82f107ceed384f1fd1
bonna023 <jerbonnardel@gmail.com>
2020-02-12 20:23:15 +0100

    init

---
+++ longest_repeated_substring.rb
@@ -0,0 +1,12 @@
+class LongestRepeatedSubstring
+
+	def longest_repeated_substring(input)
+		test_var = (input.length/2).floor
+		start_pattern_1 = 0
+		
+
+		if input[start_pattern_1..(start_pattern_1 + test_var)] == input[test_var..input]
+
+	end	
+
+end
